c46e403c9af82c9bee010d0f71f4207697625501
Collin Mullis <cmullis@saintjoe.edu>
2015-05-06 15:32:16 -0400

    Realized Mammal class was built in Human class, switched classes to work.

--- FinalProjectCRM/src/edu/saintjoe/cs/cmullis/finalprojectcrm/Human.java
+++ FinalProjectCRM/src/edu/saintjoe/cs/cmullis/finalprojectcrm/Human.java
@@ -1,48 +1,5 @@
 package edu.saintjoe.cs.cmullis.finalprojectcrm;
 
-
-/* Coded by Collin Mullis
- * CMP112 
- * Final Exam
- */
-public class Human {
-
-	// Data members
-	private final String name;
-	private int type; // 1 = Sea 2 = Land 
-
-	}
-/*----------------------------------------------------------------------------*/
-
-	// Constructor
-	public Human(String thisName, int thisType) {
-		name = thisName;
-		type = thisType;
-	}
+public class Human extends Mammal {
 	
-	// Constructor
-	public Human(String thisName) {
-		name = thisName;
-	}
-	// Constructor
-	public Human(int thisType) {
-		type = thisType;
-	}
-	
-	// Accessor
-	public String getName() {
-		return name;
-	}
-	
-	// Accessor
-	public int getType() {
-		return type;
-	}
-	
-	// Mutator
-	public int setType(int newType) {
-		int oldType = type;
-		type = newType;
-		return oldType;
-	}
 }
--- FinalProjectCRM/src/edu/saintjoe/cs/cmullis/finalprojectcrm/Mammal.java
+++ FinalProjectCRM/src/edu/saintjoe/cs/cmullis/finalprojectcrm/Mammal.java
@@ -1,10 +1,47 @@
 package edu.saintjoe.cs.cmullis.finalprojectcrm;
 
+
+/* Coded by Collin Mullis
+ * CMP112 
+ * Final Exam
+ */
 public class Mammal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Data members
+	private final String name;
+	private int type; // 1 = Sea 2 = Land 
 
+/*----------------------------------------------------------------------------*/
+
+	// Constructor
+	public Mammal(String thisName, int thisType) {
+		name = thisName;
+		type = thisType;
 	}
-
+	
+	// Constructor
+	public Mammal(String thisName) {
+		name = thisName;
+	}
+	// Constructor
+	public Mammal(int thisType) {
+		type = thisType;
+	}
+	
+	// Accessor
+	public String getName() {
+		return name;
+	}
+	
+	// Accessor
+	public int getType() {
+		return type;
+	}
+	
+	// Mutator
+	public int setType(int newType) {
+		int oldType = type;
+		type = newType;
+		return oldType;
+	}
 }
