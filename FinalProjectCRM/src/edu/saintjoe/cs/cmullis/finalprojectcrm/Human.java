a79504508d79ffb1b37e24f11f4a20b078e1f124
Collin Mullis <cmullis@saintjoe.edu>
2015-05-06 16:00:09 -0400

    Added Constructor to Human

--- FinalProjectCRM/src/edu/saintjoe/cs/cmullis/finalprojectcrm/Human.java
+++ FinalProjectCRM/src/edu/saintjoe/cs/cmullis/finalprojectcrm/Human.java
@@ -1,14 +1,21 @@
 package edu.saintjoe.cs.cmullis.finalprojectcrm;
 
-
+/* Coded by Collin Mullis
+ * CMP112
+ * Final Exam
+ */
 // SubClass for Mammal
 public class Human extends Mammal {
 
+	private String name;
 	
-	public Human(String name, int type) {
-		super(name, type);
+	public Human(String type, int age) {
+		super(type, age);
 		
 	} 
 
+	public Human(String thisName) {
+		name = thisName;
+	}
 	
 }
